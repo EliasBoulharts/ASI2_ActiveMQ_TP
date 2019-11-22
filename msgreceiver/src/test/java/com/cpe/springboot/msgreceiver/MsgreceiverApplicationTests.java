7611a9b3d6a6877acd8a9e437aa4d9c11aae7e92
eliasb <elias.boulharts@cpe.fr>
2019-11-22 11:43:26 +0100

    step 2

---
+++ msgreceiver/src/test/java/com/cpe/springboot/msgreceiver/MsgreceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpe.springboot.msgreceiver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgreceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
