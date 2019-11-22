4bd6eb8a521caeeecdd71326a49bc75e44b0186e
eliasb <elias.boulharts@cpe.fr>
2019-11-22 11:42:21 +0100

    step 1

---
+++ msgemitter/src/test/java/com/cpe/springboot/msgemitter/MsgemitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpe.springboot.msgemitter;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgemitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
